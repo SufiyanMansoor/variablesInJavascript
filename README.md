1a330639a162f7acf6d51c58b7dec890fb2db463
SufiyanMansoor <sufiyanbaw1256@gmail.com>
2020-08-23 16:34:04 +0500

    to store and create different types of variables

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # variablesInJavascript
 variables in java script
+
+in this document we read how to store values in  variables concepts in  javascript
---
+++ variablesInJs.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Sufiyan Bawany on 8/23/2020.
+ */
+
+
+var price1,price2;
+
+price1=1500+"$";
+price2=1000+"U";
+
+document.getElementById('demo').innerHTML="The First List Price is"+price1;
+document.getElementById('demo1').innerHTML="The Second List Price Is"+price2;
